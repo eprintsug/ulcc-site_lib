6503ac704d83eb600038d2f5595f06f632d8fa38
Will Fyson <will.fyson@cosector.com>
2022-05-12 10:07:14 +0000

    Plan S license default

---
+++ cfg.d/zzz_plan_s.pl
@@ -0,0 +1,9 @@
+$c->{plan_s_set_document_defaults} = $c->{set_document_defaults};
+$c->{set_document_defaults} = sub
+{
+    my( $data, $repository, $eprint ) = @_;
+
+    $repository->call( "plan_s_set_document_defaults", $data, $repository, $eprint );
+    
+    $data->{license} = "cc_by_4";
+};
