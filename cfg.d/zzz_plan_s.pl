d30754d13bf6d9ccb210344a55b7067d5e346571
Will Fyson <will.fyson@cosector.com>
2022-05-20 10:49:30 +0000

    Only apply default license to certain plan s item types

--- cfg.d/zzz_plan_s.pl
+++ cfg.d/zzz_plan_s.pl
@@ -1,9 +1,15 @@
+# Item types the UKRI OA policy applies to
+$c->{plan_s}->{item_types} = ['article', 'conference_item'];
+
 $c->{plan_s_set_document_defaults} = $c->{set_document_defaults};
 $c->{set_document_defaults} = sub
 {
     my( $data, $repository, $eprint ) = @_;
 
     $repository->call( "plan_s_set_document_defaults", $data, $repository, $eprint );
-    
-    $data->{license} = "cc_by_4" if !defined $data->{license};
+ 
+    if( grep { $eprint->value( "type" ) eq $_ } @{$repository->config( "plan_s", "item_types" )} )
+    {
+        $data->{license} = "cc_by_4" if !defined $data->{license};
+    }
 };
