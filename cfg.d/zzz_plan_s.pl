a26d5b5da5ed947dcdfcacded99a6e73b8129c65
Will Fyson <will.fyson@cosector.com>
2022-06-13 18:44:55 +0000

    Bug fix - check we actually have an eprint at this stage we can check the type of

--- cfg.d/zzz_plan_s.pl
+++ cfg.d/zzz_plan_s.pl
@@ -8,7 +8,7 @@ $c->{set_document_defaults} = sub
 
     $repository->call( "plan_s_set_document_defaults", $data, $repository, $eprint );
  
-    if( grep { $eprint->value( "type" ) eq $_ } @{$repository->config( "plan_s", "item_types" )} )
+    if( defined $eprint && grep { $eprint->value( "type" ) eq $_ } @{$repository->config( "plan_s", "item_types" )} )
     {
         $data->{license} = "cc_by_4" if !defined $data->{license};
     }
