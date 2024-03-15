26a414b448e8356a85ce05cda9e78aa50a193138
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2024-03-15 16:13:17 +0000

    remove contact_email as lives in flavours/pub_lib

--- cfg.d/zzz_ulcc.pl
+++ cfg.d/zzz_ulcc.pl
@@ -52,7 +52,8 @@ $c->{plugins}->{"Export::RSS2"}->{params}->{name} = "RSS";
 
 # Stop eprints from publicly displaying personal information in exports 
 
-push @{ $c->{fields}->{eprint} },  { name=>"contact_email", type=>"email", required=>0, can_clone=>0, export_as_xml=>0, replace_core=>1 };
+## Now in flabour
+#push @{ $c->{fields}->{eprint} },  { name=>"contact_email", type=>"email", required=>0, can_clone=>0, export_as_xml=>0, replace_core=>1 };
 
 #Fix ordering for creators_name
 #Sticking familyname and given name together ordering as if a single string results in nonsense eg:
